d1cc46a3387e28aa34bf9d2b0c77b54390532a94
Blake <blake@Blakes-MacBook-Pro.local>
2022-02-20 13:08:28 +1100

    Jess changes

--- src/reducers/yearReducer.js
+++ src/reducers/yearReducer.js
@@ -5,9 +5,7 @@ const yearReducer = (state = [], action) => {
 
     case 'ADD_YEAR': 
         console.log("Item has been added");
-        return [...state, {year: action.item}]
-        // return {...state, action.item:{}};
-        // return Object.assign({}, state, {year: action.item})
+        return [...state, action.item]
 
     case 'DELETE_ITEM':
         console.log("item deleted");
@@ -15,11 +13,54 @@ const yearReducer = (state = [], action) => {
         return state;
 
     case 'ADD_YEAR_CAT':
-        return [...state, {year: "2025", category: action.item}]
+        return [...state, action.item]
 
     default: 
         return state;
     }
 }
 
+//pass the whole thing in to add a whole new object
+// action.item = {year: "2021", cat: "shoes"}
+
 export default yearReducer;
+
+//adding a new key-value pair
+action = "something"
+
+state.map(obj => {
+    if (obj.year === 2023) {
+        obj = {...obj, cat: action} //add or update
+    }
+    return obj
+})
+
+//adding a new array
+action = [mon, tues]
+
+state.map(obj => {
+    if (obj.year === 2023) {
+        obj = {...obj, cat: action} //add
+    }
+    return obj
+})
+
+//updating an array
+action = [mon, tues]
+
+state.map(obj => {
+    if (obj.year === 2023) {
+        obj = {...obj, cat: [...obj.cat, ...action]} //update
+    }
+    return obj
+})
+
+// to add object (multiple key/value pairs)
+action = {cat: dfkj, asdjh: skdfh}
+
+state.map(obj => {
+    if (obj.year === 2023) {
+        obj = {...obj, ...action} //add or update
+    }
+    return obj
+})
