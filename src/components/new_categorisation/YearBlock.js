415891d106b5b25c28b3e16064c10f8803da1fd3
Blake <blakemcd@gmail.com>
2022-03-05 12:49:05 +1100

    setting up new approach

---
+++ src/components/new_categorisation/YearBlock.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function YearBlock() {
+  return (
+    <div>YearBlock</div>
+  )
+}
+
+export default YearBlock
+
+
+ //DISPATCHES
+    // const addYearToStore = () => {
+    //     dispatch(addYear("2023"));
+    // }
+
+    // const addYearCatToStore = () => {
+    //     dispatch(addYearCat("Finance"));
+    // }
