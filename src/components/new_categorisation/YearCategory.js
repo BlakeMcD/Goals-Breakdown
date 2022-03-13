7a800c527deac828b970b54b19f19f7ef4f5034c
Blake <blakemcd@gmail.com>
2022-03-13 19:42:07 +1100

    getting soooo cloooooooose

--- src/components/new_categorisation/YearCategory.js
+++ src/components/new_categorisation/YearCategory.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addYearCategory } from './../../actions/actionCreator';
 import { addYearCategoryItem } from '../../actions/actionCreator';
 import CategoryIcon from './CategoryIcon';
@@ -10,11 +10,31 @@ function YearCategory(props) {
     //DISPATCH
     const dispatch = useDispatch();
 
+    //SELECTOR
+    const categoryItemsArray = useSelector((state) => {
+
+        const rightObject = state.years[0].categories.find((catBlock) => catBlock.category === props.categoryName)
+
+        return rightObject.items
+    })
+    console.log(`categoryItemsArray for ${props.categoryName}`)
+    console.log(categoryItemsArray)
+
     //FUNCTIONS
     const addItemToCategory = () => {
         console.log("addItemToCategory is running")
         dispatch(addYearCategoryItem({category: props.categoryName}));
     }
+
+    const renderItemCategories = () => {
+        console.log("renderItemCategories ran") 
+        let allItems = [];
+        // for (let i = 0; i < categoriesArray.length; i++) {
+        //   allItems.push(
+        //     <YearCategory key={i} categoryName={categoriesArray[i].category}></YearCategory>
+        // )}
+        return allItems
+    } 
 
     //RETURN
     return (
